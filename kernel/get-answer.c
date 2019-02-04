957012ff6c365d1637b08203f42f1507f770dffc
Mike Rapoport <rppt@linux.ibm.com>
2019-02-04 16:45:58 +0200

    dev: ipti: mark get_answer system call as isolated
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- kernel/get-answer.c
+++ kernel/get-answer.c
@@ -1,9 +1,9 @@
+#include <asm/syscall.h>
 #include <linux/kernel.h>
 #include <linux/pid.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE0(get_answer)
+IPTI_SYSCALL_DEFINE0(get_answer)
 {
-	pr_info("%s\n", __func__);
 	return 42;
 }
