2ce4e53a66af0e6a6faf27bca708817d882a78ab
Mike Rapoport <rppt@linux.ibm.com>
2019-02-17 15:38:43 +0200

    dev: ipti: add ipti_write syscall
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- kernel/get-answer.c
+++ kernel/get-answer.c
@@ -7,3 +7,24 @@ IPTI_SYSCALL_DEFINE0(get_answer)
 {
 	return 42;
 }
+
+#define BUF_SIZE 1024
+
+IPTI_SYSCALL_DEFINE3(ipti_write, unsigned int, fd, const char __user *, ubuf,
+		     size_t, count)
+{
+	char buf[BUF_SIZE];
+
+	outb('a', 0x3f8);
+
+	if (!ubuf || count >= BUF_SIZE)
+		return -EINVAL;
+
+	buf[count] = '\0';
+	if (copy_from_user(buf, ubuf, count))
+		return -EFAULT;
+
+	pr_info("%s\n", buf);
+
+	return count;
+}
