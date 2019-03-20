417501968371d3e0a1026c1a1de83f842cf7f378
Mike Rapoport <rppt@linux.ibm.com>
2019-03-20 08:46:58 +0200

    dev: ipti: add ipti_write_bad syscall
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- kernel/get-answer.c
+++ kernel/get-answer.c
@@ -1,6 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/pid.h>
 #include <linux/syscalls.h>
+#include <linux/hugetlb.h>
+#include <asm/special_insns.h>
 
 SYSCALL_DEFINE0(get_answer)
 {
@@ -8,6 +10,24 @@ SYSCALL_DEFINE0(get_answer)
 }
 
 #define BUF_SIZE 1024
+
+static inline void check_entry_cr3(char ch)
+{
+	char yn = (__native_read_cr3() & (1 << PTI_PGTABLE_SWITCH_BIT2)) ? 'y' : 'n';
+
+	outb('=', 0x3f8);
+	outb('=', 0x3f8);
+	outb('=', 0x3f8);
+	outb('>', 0x3f8);
+	outb(' ', 0x3f8);
+	outb(ch, 0x3f8);
+	outb(':', 0x3f8);
+	outb(yn, 0x3f8);
+	outb('\n', 0x3f8);
+}
+
+typedef void (*foo)(void);
+
 
 SYSCALL_DEFINE2(ipti_write, const char __user *, ubuf, size_t, count)
 {
@@ -21,6 +41,34 @@ SYSCALL_DEFINE2(ipti_write, const char __user *, ubuf, size_t, count)
 		return -EFAULT;
 
 	pr_info("%s\n", buf);
+
+	return count;
+}
+
+SYSCALL_DEFINE2(ipti_write_bad, const char __user *, ubuf, size_t, count)
+{
+	unsigned long addr = (unsigned long)(void *)hugetlb_reserve_pages;
+	char buf[BUF_SIZE];
+	foo func1;
+
+	check_entry_cr3('a');
+
+	addr += (0xffffffff81199495 - 0xffffffff811993d0);
+	func1 = (foo)(void *)addr;
+	func1();
+
+	if (!ubuf || count >= BUF_SIZE)
+		return -EINVAL;
+
+	buf[count] = '\0';
+	if (copy_from_user(buf, ubuf, count))
+		return -EFAULT;
+
+	check_entry_cr3('b');
+
+	pr_info("%s\n", buf);
+
+	check_entry_cr3('c');
 
 	return count;
 }
