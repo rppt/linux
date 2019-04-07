f73c3528b86538e4182fcd17f6b9aef6587b5dd8
Mike Rapoport <rppt@linux.ibm.com>
2019-04-08 00:02:21 +0300

    dev: sci: kill dump_pagetable
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- arch/x86/mm/sci.c
+++ arch/x86/mm/sci.c
@@ -29,66 +29,6 @@
 #undef pr_fmt
 #define pr_fmt(fmt)     "SCI: " fmt
 
-static int bad_address(void *p)
-{
-	unsigned long dummy;
-
-	return probe_kernel_address((unsigned long *)p, dummy);
-}
-
-static void dump_pagetable(pgd_t *base, unsigned long address)
-{
-	/* pgd_t *base = __va(read_cr3_pa()); */
-	pgd_t *pgd = base + pgd_index(address);
-	p4d_t *p4d;
-	pud_t *pud;
-	pmd_t *pmd;
-	pte_t *pte;
-
-	if (bad_address(pgd))
-		goto bad;
-
-	pr_info("PGD %lx ", pgd_val(*pgd));
-
-	if (!pgd_present(*pgd))
-		goto out;
-
-	p4d = p4d_offset(pgd, address);
-	if (bad_address(p4d))
-		goto bad;
-
-	pr_cont("P4D %lx ", p4d_val(*p4d));
-	if (!p4d_present(*p4d) || p4d_large(*p4d))
-		goto out;
-
-	pud = pud_offset(p4d, address);
-	if (bad_address(pud))
-		goto bad;
-
-	pr_cont("PUD %lx ", pud_val(*pud));
-	if (!pud_present(*pud) || pud_large(*pud))
-		goto out;
-
-	pmd = pmd_offset(pud, address);
-	if (bad_address(pmd))
-		goto bad;
-
-	pr_cont("PMD %lx ", pmd_val(*pmd));
-	if (!pmd_present(*pmd) || pmd_large(*pmd))
-		goto out;
-
-	pte = pte_offset_kernel(pmd, address);
-	if (bad_address(pte))
-		goto bad;
-
-	pr_cont("PTE %lx", pte_val(*pte));
-out:
-	pr_cont("\n");
-	return;
-bad:
-	pr_info("BAD\n");
-}
-
 #define SCI_MAX_PTES 256
 #define SCI_MAX_RIPS 64
 
@@ -518,7 +458,6 @@ static int __sci_clone_pgtable(struct mm_struct *mm,
 
 	if (pud_large(*pud)) {
 		pr_info("large PUD: %lx\n", addr);
-		dump_pagetable(pgdp, addr);
 		return NO_PUD;
 	} else {
 		pmd = pmd_offset(pud, addr);
