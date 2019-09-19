d0a7a6592cf02a1c7709153aa5529d0a4560767c
Mike Rapoport <rppt@linux.ibm.com>
2019-09-19 12:20:15 +0300

    arm: mm: use strict types for page table primitives
    Currently the page table primitive types are defined as u32 or u64
    depending on LPAE. Switch to using 'struct {} pxx_t' for strict type
    checking.
    
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- arch/arm/include/asm/pgtable-2level-types.h
+++ arch/arm/include/asm/pgtable-2level-types.h
@@ -12,9 +12,6 @@
 typedef u32 pteval_t;
 typedef u32 pmdval_t;
 
-#undef STRICT_MM_TYPECHECKS
-
-#ifdef STRICT_MM_TYPECHECKS
 /*
  * These are used to make use of C type-checking..
  */
@@ -31,25 +28,5 @@ typedef struct { pteval_t pgprot; } pgprot_t;
 #define __pte(x)        ((pte_t) { (x) } )
 #define __pmd(x)        ((pmd_t) { (x) } )
 #define __pgprot(x)     ((pgprot_t) { (x) } )
-
-#else
-/*
- * .. while these make it easier on the compiler
- */
-typedef pteval_t pte_t;
-typedef pmdval_t pmd_t;
-typedef pmdval_t pgd_t[2];
-typedef pteval_t pgprot_t;
-
-#define pte_val(x)      (x)
-#define pmd_val(x)      (x)
-#define pgd_val(x)	((x)[0])
-#define pgprot_val(x)   (x)
-
-#define __pte(x)        (x)
-#define __pmd(x)        (x)
-#define __pgprot(x)     (x)
-
-#endif /* STRICT_MM_TYPECHECKS */
 
 #endif	/* _ASM_PGTABLE_2LEVEL_TYPES_H */
--- arch/arm/include/asm/pgtable-3level-types.h
+++ arch/arm/include/asm/pgtable-3level-types.h
@@ -14,10 +14,6 @@ typedef u64 pteval_t;
 typedef u64 pmdval_t;
 typedef u64 pgdval_t;
 
-#undef STRICT_MM_TYPECHECKS
-
-#ifdef STRICT_MM_TYPECHECKS
-
 /*
  * These are used to make use of C type-checking..
  */
@@ -35,24 +31,5 @@ typedef struct { pteval_t pgprot; } pgprot_t;
 #define __pmd(x)        ((pmd_t) { (x) } )
 #define __pgd(x)	((pgd_t) { (x) } )
 #define __pgprot(x)     ((pgprot_t) { (x) } )
-
-#else	/* !STRICT_MM_TYPECHECKS */
-
-typedef pteval_t pte_t;
-typedef pmdval_t pmd_t;
-typedef pgdval_t pgd_t;
-typedef pteval_t pgprot_t;
-
-#define pte_val(x)	(x)
-#define pmd_val(x)	(x)
-#define pgd_val(x)	(x)
-#define pgprot_val(x)	(x)
-
-#define __pte(x)	(x)
-#define __pmd(x)	(x)
-#define __pgd(x)	(x)
-#define __pgprot(x)	(x)
-
-#endif	/* STRICT_MM_TYPECHECKS */
 
 #endif	/* _ASM_PGTABLE_3LEVEL_TYPES_H */
