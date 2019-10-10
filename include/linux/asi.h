c080a13d94869443c86bf6b38cbba3416614d3a2
Mike Rapoport <rppt@linux.ibm.com>
2019-10-10 10:19:47 +0300

    asi: add basic functionality for page table cloning
    Start implementation Address Space Isolation (asi) framework.
    Add the basic functionality to clone kernel page tables up to desired
    level.
    
    Co-developed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

---
+++ include/linux/asi.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef _INCLUDE_LINUX_ASI_H
+#define _INCLUDE_LINUX_ASI_H
+
+enum asi_clone_level {
+	ASI_LEVEL_LEAF,
+        ASI_LEVEL_PTE,
+        ASI_LEVEL_PMD,
+        ASI_LEVEL_PUD,
+        ASI_LEVEL_P4D,
+        ASI_LEVEL_PGD,
+};
+
+int asi_clone_pgd_range(struct mm_struct *dst_mm,
+			struct mm_struct *src_mm,
+			pgd_t *dst_pagetable, pgd_t *src_pagetable,
+			unsigned long addr, unsigned long end,
+			enum asi_clone_level level);
+
+int asi_map_range(struct mm_struct *mm, pgd_t *pgd,
+		  unsigned long virt, phys_addr_t phys,
+		  pgprot_t prot, int nr_pages);
+
+static inline int asi_map_page(struct mm_struct *mm, pgd_t *pgd,
+			       unsigned long virt, phys_addr_t phys,
+			       pgprot_t prot)
+{
+	return asi_map_range(mm, pgd, virt, phys, prot, 1);
+}
+
+#endif /* _INCLUDE_LINUX_ASI_H */
