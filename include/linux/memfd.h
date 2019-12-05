cff88ee0fca22fa9811229eca9acfcebdfb64794
Mike Rapoport <rppt@linux.ibm.com>
2019-12-05 08:29:27 +0200

    dev: secretmem: update Kconfig and Makefile
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- include/linux/memfd.h
+++ include/linux/memfd.h
@@ -6,13 +6,16 @@
 
 #ifdef CONFIG_MEMFD_CREATE
 extern long memfd_fcntl(struct file *file, unsigned int cmd, unsigned long arg);
-extern struct file *secretmem_file_create(const char *name, unsigned int flags);
 #else
 static inline long memfd_fcntl(struct file *f, unsigned int c, unsigned long a)
 {
 	return -EINVAL;
 }
+#endif
 
+#ifdef CONFIG_MEMFD_SECRETMEM
+extern struct file *secretmem_file_create(const char *name, unsigned int flags);
+#else
 static inline struct file *secretmem_file_create(const char *name, unsigned int flags)
 {
 	return ERR_PTR(-EINVAL);
