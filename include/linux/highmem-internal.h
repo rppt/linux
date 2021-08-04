9ba11ab3e285b2bd4bd4b6ba746a36631067fc8b
Ira Weiny <ira.weiny@intel.com>
2021-08-03 21:32:28 -0700

    kmap: Add stray access protection for devmap pages
    Enable PKS protection for devmap pages.  The devmap protection facility
    wants to co-opt kmap_{local_page,atomic}() to mediate access to PKS
    protected pages.
    
    kmap() allows for global mappings to be established, while the PKS
    facility depends on thread-local access. For this reason kmap() is not
    supported, but it leaves a policy decision for what to do when kmap()
    is attempted on a protected devmap page.
    
    Neither of the 2 current DAX-capable filesystems (ext4 and xfs) perform
    such global mappings.  The bulk of device drivers that would handle
    devmap pages are not using kmap().  Any future filesystems that gain DAX
    support, or device drivers wanting to support devmap protected pages
    will need to move to kmap_local_page().  In the meantime to handle these
    kmap() users call pgmap_protection_flag_invalid() to flag and invalid
    use of any potentially protected pages.  This allows better debugging of
    invalided uses vs catching faults later on when the address is used.
    
    Direct-map exposure is already mitigated by default on HIGHMEM systems
    because by definition HIGHMEM systems do not have large capacities of
    memory in the direct map.  Therefore, to reduce complexity HIGHMEM
    systems are not supported.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Link: https://lore.kernel.org/r/20210804043231.2655537-16-ira.weiny@intel.com

--- include/linux/highmem-internal.h
+++ include/linux/highmem-internal.h
@@ -142,6 +142,7 @@ static inline struct page *kmap_to_page(void *addr)
 static inline void *kmap(struct page *page)
 {
 	might_sleep();
+	pgmap_protection_flag_invalid(page);
 	return page_address(page);
 }
 
@@ -157,6 +158,7 @@ static inline void kunmap(struct page *page)
 
 static inline void *kmap_local_page(struct page *page)
 {
+	pgmap_mk_readwrite(page);
 	return page_address(page);
 }
 
@@ -175,12 +177,14 @@ static inline void __kunmap_local(void *addr)
 #ifdef ARCH_HAS_FLUSH_ON_KUNMAP
 	kunmap_flush_on_unmap(addr);
 #endif
+	pgmap_mk_noaccess(kmap_to_page(addr));
 }
 
 static inline void *kmap_atomic(struct page *page)
 {
 	preempt_disable();
 	pagefault_disable();
+	pgmap_mk_readwrite(page);
 	return page_address(page);
 }
 
@@ -199,6 +203,7 @@ static inline void __kunmap_atomic(void *addr)
 #ifdef ARCH_HAS_FLUSH_ON_KUNMAP
 	kunmap_flush_on_unmap(addr);
 #endif
+	pgmap_mk_noaccess(kmap_to_page(addr));
 	pagefault_enable();
 	preempt_enable();
 }
