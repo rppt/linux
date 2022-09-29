a4aab609dec254028bea0b4614d8abb80e43fec2
Yu-cheng Yu <yu-cheng.yu@intel.com>
2022-09-29 15:29:16 -0700

    mm/mmap: Add shadow stack pages to memory accounting
    Account shadow stack pages to stack memory.
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Cc: Kees Cook <keescook@chromium.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1682,6 +1682,9 @@ static inline int accountable_mapping(struct file *file, vm_flags_t vm_flags)
 	if (file && is_file_hugepages(file))
 		return 0;
 
+	if (vm_flags & VM_SHADOW_STACK)
+		return 1;
+
 	return (vm_flags & (VM_NORESERVE | VM_SHARED | VM_WRITE)) == VM_WRITE;
 }
 
@@ -3288,6 +3291,8 @@ void vm_stat_account(struct mm_struct *mm, vm_flags_t flags, long npages)
 	if (is_exec_mapping(flags))
 		mm->exec_vm += npages;
 	else if (is_stack_mapping(flags))
+		mm->stack_vm += npages;
+	else if (flags & VM_SHADOW_STACK)
 		mm->stack_vm += npages;
 	else if (is_data_mapping(flags))
 		mm->data_vm += npages;
