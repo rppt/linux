a94ef483695b78b6b85d6fa96c22c38267537777
Mike Rapoport (IBM) <rppt@kernel.org>
2023-06-16 11:50:35 +0300

    powerpc: extend execmem_params for kprobes allocations
    powerpc overrides kprobes::alloc_insn_page() to remove writable
    permissions when STRICT_MODULE_RWX is on.
    
    Add definition of EXECMEM_KRPOBES to execmem_params to allow using the generic
    kprobes::alloc_insn_page() with the desired permissions.
    
    As powerpc uses breakpoint instructions to inject kprobes, it does not
    need to constrain kprobe allocations to the modules area and can use the
    entire vmalloc address space.
    
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

--- arch/powerpc/kernel/module.c
+++ arch/powerpc/kernel/module.c
@@ -95,6 +95,9 @@ static struct execmem_params execmem_params __ro_after_init = {
 		[EXECMEM_MODULE_TEXT] = {
 			.alignment = 1,
 		},
+		[EXECMEM_KPROBES] = {
+			.alignment = 1,
+		},
 		[EXECMEM_MODULE_DATA] = {
 			.alignment = 1,
 		},
@@ -135,6 +138,14 @@ struct execmem_params __init *execmem_arch_params(void)
 #endif
 
 	text->pgprot = prot;
+
+	execmem_params.ranges[EXECMEM_KPROBES].start = VMALLOC_START;
+	execmem_params.ranges[EXECMEM_KPROBES].start = VMALLOC_END;
+
+	if (strict_module_rwx_enabled())
+		execmem_params.ranges[EXECMEM_KPROBES].pgprot = PAGE_KERNEL_ROX;
+	else
+		execmem_params.ranges[EXECMEM_KPROBES].pgprot = PAGE_KERNEL_EXEC;
 
 	return &execmem_params;
 }
