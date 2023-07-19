3f43108081fbbfda0a07ecddb6f520a8887821c9
Mike Rapoport (IBM) <rppt@kernel.org>
2023-07-19 11:46:34 +0300

    dev: execmem: start implementing maple_tree based cache
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

--- include/linux/execmem.h
+++ include/linux/execmem.h
@@ -49,6 +49,7 @@ enum execmem_type {
 enum execmem_range_flags {
 	EXECMEM_KASAN_SHADOW	= (1 << 0),
 	EXECMEM_UNMAPPED	= (1 << 1),
+	EXECMEM_CACHED		= (1 << 2),
 };
 
 /**
--- mm/execmem.c
+++ mm/execmem.c
@@ -1,13 +1,310 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/mm.h>
+#include <linux/list.h>
 #include <linux/vmalloc.h>
 #include <linux/execmem.h>
+#include <linux/spinlock.h>
+#include <linux/set_memory.h>
+#include <linux/maple_tree.h>
 #include <linux/moduleloader.h>
 
 #include "internal.h"
 
+/* FIXME: have PMD_ORDER at last available in include/linux */
+#define PMD_ORDER	(PMD_SHIFT - PAGE_SHIFT)
+#define MODULES_ALIGN(x)	ALIGN((x), MODULE_ALIGN)
+
+/* #define FREE_AREA_COUNT (PMD_SIZE/MODULE_ALIGN + 1) */
+#define FREE_AREA_COUNT 513
+
 static struct execmem_params execmem_params;
+struct exm_area {
+	unsigned long start;
+	size_t size;
+	struct list_head list;
+};
+
+struct execmem_free_list {
+	struct list_head head;
+};
+
+struct execmem_cache {
+	spinlock_t lock;
+	struct maple_tree busy_mt;
+	struct maple_tree free_mt;
+	struct execmem_free_list free_area[FREE_AREA_COUNT];
+};
+
+static struct execmem_cache execmem_cache = {
+	.busy_mt = MTREE_INIT(busy_mt, 0),
+	.free_mt = MTREE_INIT(free_mt, 0),
+};
+
+static void execmem_cache_dump(void)
+{
+	struct exm_area *area;
+	unsigned long index = 0;
+	void *entry;
+
+	pr_info("free maple tree:\n");
+	mt_for_each(&execmem_cache.free_mt, area, index, ULONG_MAX) {
+		pr_info("i: %lx addr: %lx end: %lx size: %lx\n", index & ~7, area->start, area->start + area->size, area->size);
+	}
+
+	pr_info("free lists:\n");
+	for (int i = 0; i < ARRAY_SIZE(execmem_cache.free_area); i++) {
+		struct execmem_free_list *list = &execmem_cache.free_area[i];
+
+		if (!list) {
+			pr_info("i: %d NULL\n", i);
+			continue;
+		}
+
+		if (list_empty(&list->head))
+			continue;
+
+		pr_info("i: %d:", i);
+		list_for_each_entry(area, &list->head, list) {
+			pr_info("\taddr: %lx end: %lx size: %lx\n", area->start, area->start + area->size, area->size);
+		}
+	}
+
+	index = 0;
+	pr_info("busy maple tree:\n");
+	mt_for_each(&execmem_cache.busy_mt, entry, index, ULONG_MAX) {
+		unsigned long addr = index & ~7;
+		size_t size;
+
+		if (!xa_is_value(entry)) {
+			pr_info("not value at %lx\n", index);
+			continue;
+		}
+
+		size = xa_to_value(entry);
+		pr_info("addr: %lx end: %lx size: %lx\n", addr, addr + size, size);
+	}
+}
+
+#define bug_on(cond)					\
+	if ((cond)) {					\
+		execmem_cache_dump();			\
+		BUG();					\
+	}
+
+static int execmem_cache_add(void *ptr, size_t size)
+{
+	struct maple_tree *mt = &execmem_cache.free_mt;
+	unsigned long addr = (unsigned long)ptr;
+	struct exm_area *entry, *upper, *lower;
+	struct execmem_free_list *free_list;
+	unsigned long index, lower_index, upper_index;
+	size_t lower_sz = 0, upper_sz = 0;
+	bool merge_prev = false;
+	bool merge_next = false;
+	size_t asize;
+	int err;
+
+	pr_debug("%s: adding %lx at %p\n", __func__, size, ptr);
+
+	index = 0;
+	lower = mt_find(mt, &index, addr - 1);
+	if (lower) {
+		lower_sz = lower->size;
+		lower_index = index;
+
+		if (index + lower_sz == addr) {
+			lower->size = size + lower_sz;
+			list_del_init(&lower->list);
+
+			err = mtree_store(mt, lower_index, lower, GFP_KERNEL);
+			bug_on(err);
+
+			merge_prev = true;
+		}
+	}
+
+	index++;
+	upper = mt_find_after(mt, &index, ULONG_MAX);
+	if (upper) {
+		upper_sz = upper->size;
+		upper_index = index;
+
+		if (addr + size == upper_index) {
+			unsigned long sz = size + upper_sz;
+
+			list_del_init(&upper->list);
+			mtree_erase(mt, upper_index);
+
+			if (merge_prev) {
+				sz += lower_sz;
+			} else {
+				index = addr;
+			}
+
+			upper->size = sz;
+			upper->start = index;
+			entry = upper;
+
+			err = mtree_store(mt, index, entry, GFP_KERNEL);
+			bug_on(err);
+
+			merge_next = true;
+		}
+	}
+
+	if (!merge_prev && !merge_next) {
+		entry = kzalloc(sizeof(*entry), GFP_KERNEL);
+		entry->start = addr;
+		entry->size = size;
+		INIT_LIST_HEAD(&entry->list);
+
+		err = mtree_store(mt, addr, entry, GFP_KERNEL);
+		bug_on(err);
+	}
+
+	asize = entry->size >> PAGE_SHIFT;
+	free_list = &execmem_cache.free_area[asize];
+	list_add(&entry->list, &free_list->head);
+
+	return 0;
+}
+
+static void *execmem_cache_alloc(size_t size)
+{
+	struct maple_tree *free_mt = &execmem_cache.free_mt;
+	struct maple_tree *busy_mt = &execmem_cache.busy_mt;
+	size_t asize = size >> PAGE_SHIFT;
+	struct exm_area *entry, *entry1;
+	/* struct ma_state mas; */
+	void *ptr = NULL;
+
+	for (int i = asize; i < ARRAY_SIZE(execmem_cache.free_area); i++) {
+		struct list_head *head = &execmem_cache.free_area[i].head;
+		unsigned long addr;
+		int err;
+
+		if (list_empty(head))
+			continue;
+
+		entry = list_first_entry(head, struct exm_area, list);
+
+		addr = entry->start;
+		list_del_init(&entry->list);
+
+		entry1 = mtree_load(free_mt, addr);
+		bug_on(!entry1);
+		bug_on(entry != entry1);
+		mtree_erase(free_mt, addr);
+
+		if (i > asize) {
+			execmem_cache_add((void *)(entry->start + size),
+					  entry->size - size);
+		}
+
+		pr_debug("%s: busy: adding %lx at %lx\n", __func__, size, addr);
+		err = mtree_insert(busy_mt, addr, xa_mk_value(size), GFP_KERNEL);
+		if (err) {
+			pr_err("insert busy failed: %d\n", err);
+			execmem_cache_dump();
+			bug_on(err);
+		}
+
+		ptr = (void *)addr;
+		break;
+	}
+
+	return ptr;
+}
+
+static void execmem_free_cached(void *ptr)
+{
+	struct maple_tree *busy_mt = &execmem_cache.busy_mt;
+	void *entry = mtree_load(busy_mt, (unsigned long)ptr);
+	size_t size;
+
+	bug_on(!entry);
+	bug_on(!xa_is_value(entry));
+
+	size = xa_to_value(entry);
+	mtree_erase(busy_mt, (unsigned long)ptr);
+
+	execmem_cache_add(ptr, size);
+}
+
+static void execmem_cache_init(void)
+{
+	for (int i = 0; i < ARRAY_SIZE(execmem_cache.free_area); i++) {
+		struct execmem_free_list *l = &execmem_cache.free_area[i];
+
+		INIT_LIST_HEAD(&l->head);
+	}
+	spin_lock_init(&execmem_cache.lock);
+}
+
+static void *execmem_alloc_cached(size_t size, struct execmem_range *range,
+				  gfp_t gfp_flags)
+{
+	unsigned long vm_flags = VM_FLUSH_RESET_PERMS | VM_NO_GUARD;
+	unsigned long start = range->start;
+	unsigned long end = range->end;
+	unsigned int align = range->alignment;
+	pgprot_t pgprot = range->pgprot;
+	unsigned long addr;
+	size_t alloc_size;
+	void *ptr;
+	int err;
+
+	/* try the cache first */
+	size = MODULES_ALIGN(size);
+	ptr = execmem_cache_alloc(size);
+	if (ptr) {
+		__set_memory_prot((unsigned long)ptr, size, pgprot);
+		return ptr;
+	}
+
+	/* try allocating large pages that cover the requested size */
+	alloc_size = ALIGN(size, PMD_SIZE);
+	/* vm_flags |= VM_ALLOW_HUGE_VMAP; */
+	ptr =  __vmalloc_node_range(alloc_size, PMD_SIZE, start, end, gfp_flags,
+				    pgprot, vm_flags, NUMA_NO_NODE,
+				    __builtin_return_address(0));
+	if (!ptr) {
+		/* /\* if requested size cannot be allocated, nothing to do *\/ */
+		/* if (alloc_size == size) */
+		/* 	return NULL; */
+
+		/* /\* try getting exact size, screw large pages *\/ */
+		/* vm_flags &= ~VM_ALLOW_HUGE_VMAP; */
+		/* ptr =  __vmalloc_node_range(size, align, start, end, gfp_flags, */
+		/* 			     pgprot, vm_flags, NUMA_NO_NODE, */
+		/* 			     __builtin_return_address(0)); */
+		if (!ptr)
+			return NULL;
+	}
+
+	/* add extra memory (if any) to the cache */
+	if (alloc_size > size) {
+		err = execmem_cache_add(ptr + size, alloc_size - size);
+		if (err) {
+			vfree(ptr);
+			return NULL;
+		}
+	}
+
+	/* record the allocated pointer */
+	addr = (unsigned long)ptr;
+	err = mtree_insert(&execmem_cache.busy_mt, addr, xa_mk_value(size),
+			   GFP_KERNEL);
+	bug_on(err == -EEXIST);
+	if (err) {
+		/* FIXME: remove from cache */
+		vfree(ptr);
+		return NULL;
+	}
+
+	return ptr;
+}
 
 static void *execmem_alloc_unmapped(size_t size, struct execmem_range *range,
 				    gfp_t gfp_flags)
@@ -81,6 +378,9 @@ static void *__execmem_alloc(size_t size, struct execmem_range *range,
 	if (range->flags & EXECMEM_UNMAPPED)
 		return execmem_alloc_unmapped(size, range, gfp_flags);
 
+	if (range->flags & EXECMEM_CACHED)
+		return execmem_alloc_cached(size, range, gfp_flags);
+
 	return __vmalloc_node_range(size, align, start, end, gfp_flags,
 				    pgprot, vm_flags, NUMA_NO_NODE,
 				    __builtin_return_address(0));
@@ -149,6 +449,12 @@ void execmem_free(void *ptr)
 
 	for (int i = 0; i < ARRAY_SIZE(execmem_params.ranges); i++) {
 		struct execmem_range *range = &execmem_params.ranges[i];
+
+		if ((addr >= range->start && addr < range->end) &&
+		    range->flags & EXECMEM_CACHED) {
+			execmem_free_cached(ptr);
+			return;
+		}
 
 		if ((addr >= range->start && addr < range->end) &&
 		    range->flags & EXECMEM_UNMAPPED) {
@@ -229,4 +535,5 @@ void __init execmem_init(void)
 	execmem_params = *p;
 
 	execmem_init_missing(&execmem_params);
+	execmem_cache_init();
 }
