e456c97627b524790745dd944ffb08d164cfb023
Mike Rapoport (IBM) <rppt@kernel.org>
2024-03-29 09:39:29 +0300

    mm/execmem, arch: convert simple overrides of module_alloc to execmem
    Several architectures override module_alloc() only to define address
    range for code allocations different than VMALLOC address space.
    
    Provide a generic implementation in execmem that uses the parameters
    for address space ranges, required alignment and page protections
    provided by architectures.
    
    The architectures must fill execmem_info structure and implement
    execmem_arch_setup() that returns a pointer to that structure. This
    way the execmem initialization won't be called from every architecture,
    but rather from a central place, namely initialization of the core
    memory management.
    
    The execmem provides execmem_text_alloc() API that wraps
    __vmalloc_node_range() with the parameters defined by the architectures.
    If an architecture does not implement execmem_arch_setup(),
    execmem_text_alloc() will fall back to module_alloc().
    
    The name execmem_text_alloc() emphasizes that the allocated memory is
    for executable code, the allocations of the associated data, like data
    sections of a module will use execmem_data_alloc() interface that will
    be added later.
    
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

--- mm/execmem.c
+++ mm/execmem.c
@@ -5,14 +5,28 @@
 #include <linux/execmem.h>
 #include <linux/moduleloader.h>
 
-static void *execmem_alloc(size_t size)
+static struct execmem_info execmem_info;
+
+static void *execmem_alloc(struct execmem_range *range, size_t size)
 {
-	return module_alloc(size);
+	unsigned long start = range->start;
+	unsigned long end = range->end;
+	unsigned int align = range->alignment;
+	pgprot_t pgprot = range->pgprot;
+
+	return __vmalloc_node_range(size, align, start, end,
+				    GFP_KERNEL, pgprot, VM_FLUSH_RESET_PERMS,
+				    NUMA_NO_NODE, __builtin_return_address(0));
 }
 
 void *execmem_text_alloc(enum execmem_type type, size_t size)
 {
-	return execmem_alloc(size);
+	struct execmem_range *range = &execmem_info.ranges[type];
+
+	if (!range->start)
+		return module_alloc(size);
+
+	return execmem_alloc(range, size);
 }
 
 void execmem_free(void *ptr)
@@ -24,3 +38,54 @@ void execmem_free(void *ptr)
 	WARN_ON(in_interrupt());
 	vfree(ptr);
 }
+
+static bool execmem_validate(struct execmem_info *info)
+{
+	struct execmem_range *r = &info->ranges[EXECMEM_DEFAULT];
+
+	if (!r->alignment || !r->start || !r->end || !pgprot_val(r->pgprot)) {
+		pr_crit("Invalid parameters for execmem allocator, module loading will fail");
+		return false;
+	}
+
+	return true;
+}
+
+static void execmem_init_missing(struct execmem_info *info)
+{
+	struct execmem_range *default_range = &info->ranges[EXECMEM_DEFAULT];
+
+	for (int i = EXECMEM_DEFAULT + 1; i < EXECMEM_TYPE_MAX; i++) {
+		struct execmem_range *r = &info->ranges[i];
+
+		if (!r->start) {
+			r->pgprot = default_range->pgprot;
+			r->alignment = default_range->alignment;
+			r->start = default_range->start;
+			r->end = default_range->end;
+		}
+	}
+}
+
+struct execmem_info * __weak execmem_arch_setup(void)
+{
+	return NULL;
+}
+
+static int __init execmem_init(void)
+{
+	struct execmem_info *info = execmem_arch_setup();
+
+	if (!info)
+		return 0;
+
+	if (!execmem_validate(info))
+		return -EINVAL;
+
+	execmem_init_missing(info);
+
+	execmem_info = *info;
+
+	return 0;
+}
+core_initcall(execmem_init);
