ccb063ed7fdbbf16d2a638e5af2a3cb2930461e1
Mike Rapoport (IBM) <rppt@kernel.org>
2024-06-13 11:24:48 +0300

    dev: introduce mm/numa_memblks
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

---
+++ include/linux/numa_memblks.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __NUMA_MEMBLKS_H
+#define __NUMA_MEMBLKS_H
+
+#ifdef CONFIG_NUMA_MEMBLKS
+#include <linux/types.h>
+
+#define NR_NODE_MEMBLKS		(MAX_NUMNODES * 2)
+
+struct numa_memblk {
+	u64			start;
+	u64			end;
+	int			nid;
+};
+
+struct numa_meminfo {
+	int			nr_blks;
+	struct numa_memblk	blk[NR_NODE_MEMBLKS];
+};
+
+extern struct numa_meminfo numa_meminfo __initdata_or_meminfo;
+extern struct numa_meminfo numa_reserved_meminfo __initdata_or_meminfo;
+
+void __init numa_remove_memblk_from(int idx, struct numa_meminfo *mi);
+int __init numa_cleanup_meminfo(struct numa_meminfo *mi);
+
+int __init numa_add_memblk(int nodeid, u64 start, u64 end);
+
+int __init numa_register_meminfo(struct numa_meminfo *mi);
+
+extern int numa_distance_cnt;
+void __init numa_reset_distance(void);
+
+#endif /* CONFIG_NUMA_MEMBLKS */
+
+#endif	/* __NUMA_MEMBLKS_H */
