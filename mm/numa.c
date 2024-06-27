695822b7a8af77dbb3e5a0c659246ff1d472eac6
Mike Rapoport (IBM) <rppt@kernel.org>
2024-06-27 13:40:43 +0300

    dev: numa: pull out alloc_node_data t generic code
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

--- mm/numa.c
+++ mm/numa.c
@@ -7,6 +7,37 @@
 #include <linux/printk.h>
 #include <linux/sort.h>
 
+/* Allocate NODE_DATA for a node on the local memory */
+void __init alloc_node_data(int nid)
+{
+	const size_t nd_size = roundup(sizeof(pg_data_t), PAGE_SIZE);
+	u64 nd_pa;
+	void *nd;
+	int tnid;
+
+	/*
+	 * Allocate node data.  Try node-local memory and then any node.
+	 * Never allocate in DMA zone.
+	 */
+	nd_pa = memblock_phys_alloc_try_nid(nd_size, SMP_CACHE_BYTES, nid);
+	if (!nd_pa)
+		panic("Cannot allocate %zu bytes for node %d data\n",
+		      nd_size, nid);
+	nd = __va(nd_pa);
+
+	/* report and initialize */
+	printk(KERN_INFO "NODE_DATA(%d) allocated [mem %#010Lx-%#010Lx]\n", nid,
+	       nd_pa, nd_pa + nd_size - 1);
+	tnid = early_pfn_to_nid(nd_pa >> PAGE_SHIFT);
+	if (tnid != nid)
+		printk(KERN_INFO "    NODE_DATA(%d) on node %d\n", nid, tnid);
+
+	node_data[nid] = nd;
+	memset(NODE_DATA(nid), 0, sizeof(pg_data_t));
+
+	node_set_online(nid);
+}
+
 #ifndef CONFIG_NUMA_KEEP_MEMINFO
 
 /* Stub functions: */
