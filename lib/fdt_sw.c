874f8ade4663a9cf9f1e4ea56727c1ee72438ded
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-01-22 10:16:25 +0200

    dev: lib/fdt: make some of fdt API available to modules
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- lib/fdt_sw.c
+++ lib/fdt_sw.c
@@ -1,2 +1,10 @@
 #include <linux/libfdt_env.h>
+#include <linux/module.h>
 #include "../scripts/dtc/libfdt/fdt_sw.c"
+
+EXPORT_SYMBOL_GPL(fdt_begin_node);
+EXPORT_SYMBOL_GPL(fdt_end_node);
+EXPORT_SYMBOL_GPL(fdt_getprop);
+EXPORT_SYMBOL_GPL(fdt_node_check_compatible);
+EXPORT_SYMBOL_GPL(fdt_path_offset);
+EXPORT_SYMBOL_GPL(fdt_property);
