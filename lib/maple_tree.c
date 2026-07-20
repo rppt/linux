e0f013b8ea8f0db232ab76c481f9c40de441592c
Liam R. Howlett (Oracle) <liam@infradead.org>
2026-07-20 17:30:38 -0400

    maple_tree-stop-flooding-logs-when-debug_locks-is-set-to-zero-fix
    stop flooding logs when debug_locks is set to zero
    
    When lockdep detects an issue, it sets debug_locks to 0 disabling further
    reports.  The newly added maple tree sequence counting on locks to detect
    errors is not taking this nor the fact that lock_is_held() can return -1
    (unknown state) into account.  The resulting action is a flood of logging
    of not matching sequence numbers being reported on failure.
    
    __lock_sequnece() will return u32 ~0 when debug_locks is zero, and the
    real sequnece count cannot return such a high value as it is less than
    32bits.
    
    By always updating the sequence number, regardless of lock state and by
    ignoring ~0 value in the sequence number will avoid ever printing a
    WARN_ON when this condition is hit.
    
    Link: https://lore.kernel.org/fo5r2xl3fql6gyvjasfvfu4ftugjti2vxg7zskdw7hjeo3xvqm@kk66qmsf762s
    Signed-off-by: Liam R. Howlett (Oracle) <liam@infradead.org>
    Tested-by: Breno Leitao <leitao@debian.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Chris Mason <clm@meta.com>
    Cc: Chuck Lever <cel@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Joe Perches <joe@perches.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -1171,6 +1171,8 @@ static void mas_lock_check(struct ma_state *mas)
 {
 #ifdef CONFIG_LOCKDEP
 	struct lockdep_map *map;
+	u32 seq;
+
 	if (!mas_is_active(mas))
 		return;
 
@@ -1182,8 +1184,12 @@ static void mas_lock_check(struct ma_state *mas)
 #endif /* CONFIG_RCU_STRICT_GRACE_PERIOD */
 
 	map = mas_lockdep_map(mas);
-	if (map && lock_is_held(map))
-		WARN_ON_ONCE(mas->ld_seq != lock_sequence(map));
+	if (!map)
+		return;
+
+	seq = lock_sequence(map);
+	if (seq != UINT_MAX && mas->ld_seq != UINT_MAX)
+		WARN_ON_ONCE(mas->ld_seq != seq);
 #endif /* CONFIG_LOCKDEP */
 
 }
@@ -1201,8 +1207,8 @@ static void mas_init_lock_check(struct ma_state *mas)
 #endif /* CONFIG_RCU_STRICT_GRACE_PERIOD */
 
 	map = mas_lockdep_map(mas);
-	if (map && lock_is_held(map))
-		mas->ld_seq = lock_sequence(mas_lockdep_map(mas));
+	if (map) /* Update regardless of lock state */
+		mas->ld_seq = lock_sequence(map);
 #endif /* CONFIG_LOCKDEP */
 
 }
