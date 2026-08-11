a3fe643429305853fc6c6ce5776f462626c83a47
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-08-11 13:51:59 +0300

    set_memory: add number of pages parameter to set_direct_map APIs
    When set_direct_map APIs were introduced by the commit d253ca0c3865
    ("x86/mm/cpa: Add set_direct_map_*() functions") the single page
    parameter made sense because the initial callers (vmalloc and
    hibernation) had sets of unsorted struct pages that required changes of
    their mappings in the direct map.
    
    Since there is an increasing demand for direct map manipulation and it
    is also desirable to be able to update larger physically contiguous
    mappings, for example an entire large folio, extend set_direct_map APIs
    to receive number of pages parameter.
    
    As there is still only a handful of callers, change the existing
    functions directly and update all the call sites rather than adding
    wrappers for single page case.
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- arch/x86/mm/pat/set_memory.c
+++ arch/x86/mm/pat/set_memory.c
@@ -2673,14 +2673,14 @@ static int __set_pages_np(struct page *page, int numpages)
 	return __change_page_attr_set_clr(&cpa, 1);
 }
 
-int set_direct_map_invalid_noflush(struct page *page)
+int set_direct_map_invalid_noflush(struct page *page, unsigned int nr)
 {
-	return __set_pages_np(page, 1);
+	return __set_pages_np(page, nr);
 }
 
-int set_direct_map_default_noflush(struct page *page)
+int set_direct_map_default_noflush(struct page *page, unsigned int nr)
 {
-	return __set_pages_p(page, 1);
+	return __set_pages_p(page, nr);
 }
 
 int set_direct_map_valid_noflush(struct page *page, unsigned nr, bool valid)
